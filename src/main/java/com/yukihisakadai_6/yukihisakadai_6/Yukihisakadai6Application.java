796ef3385d38e13f191ef071ef8d510a6e6b56b5
和田幸久 <yuki777_march@yahoo.co.jp>
2024-03-12 16:45:34 +0900

    プロジェクトのセットアップ

---
+++ src/main/java/com/yukihisakadai_6/yukihisakadai_6/Yukihisakadai6Application.java
@@ -0,0 +1,13 @@
+package com.yukihisakadai_6.yukihisakadai_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yukihisakadai6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yukihisakadai6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yukihisakadai_6/yukihisakadai_6/Yukihisakadai6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukihisakadai_6.yukihisakadai_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yukihisakadai6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
